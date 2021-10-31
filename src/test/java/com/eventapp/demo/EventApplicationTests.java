2c60d0066753faeda956ba0c8985d27bf55cf52c
ian-kohl <ianvictor181099@gmail.com>
2021-10-31 20:09:24 -0300

    first commit

---
+++ src/test/java/com/eventapp/demo/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
